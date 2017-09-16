26284aec1efda6e1c16e42858dec3dadcd0ce9a0
zengkun <zengkun0415@sina.com>
2017-09-16 21:49:52 +0800

    Test.vue增加组件的注册

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,12 +1,42 @@
 <template>
-    <Button type="primary" @click="info">显示普通提醒</Button>
+    <div>
+        <Button type="primary" @click="info">显示普通提醒</Button>
+        <anchored-heading :level='1'>标题</anchored-heading>
+    </div>
 </template>
 <script>
-    export default {
-        methods: {
-            info () {
-                this.$Message.info('这是一条普通的提醒');
-            }
+// 方式1，注册（全局）组件（render函数）
+// 也可以在main.js中直接注册
+// import Vue from 'vue';
+// Vue.component('anchored-heading', {
+//   render: function (createElement) {
+//     return createElement(
+//       'h' + this.level,   // tag name 标签名称
+//       this.$slots.default // 子组件中的阵列
+//     )
+//   },
+//   props: {
+//     level: {
+//       type: Number,
+//       required: true
+//     }
+//   }
+// })
+
+export default {
+    methods: {
+        info() {
+            this.$Message.info('这是一条普通的提醒');
         }
-    }
+    },
+   	// 方式2，注册局部组件（render函数）
+    components: {
+        'anchored-heading': {
+            render: function(createElement) {
+                return createElement(
+                    'h' + this.level, // tag name 标签名称
+                    this.$slots.default // 子组件中的阵列
+                )
+            },
+        }
 </script>
